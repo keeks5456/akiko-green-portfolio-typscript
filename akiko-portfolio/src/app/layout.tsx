bd3829c089a165027add31f71561cb5c05c810f7
Akiko Green <akikogreen08@gmail.com>
2024-01-04 19:55:55 -0800

    nav bar is responsive

--- akiko-portfolio/src/app/layout.tsx
+++ akiko-portfolio/src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/navigation/navbar";
+import Navigation from "./components/navigation/page";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
@@ -19,7 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        <Navigation />
         {children}
       </body>
     </html>
