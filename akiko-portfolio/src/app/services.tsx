5f9ba253596b7f3407347707e293767a1fbaffa5
Akiko Green <akikogreen08@gmail.com>
2024-01-14 20:56:37 -0800

    adds more data to project object

--- akiko-portfolio/src/app/services.tsx
+++ akiko-portfolio/src/app/services.tsx
@@ -5,30 +5,40 @@ export const projectData = [
     image: "/images/flower.png",
     title: "QPQ",
     github: "https://github.com/bdb2381/qpq-frontend",
-    demo: "",
+    technologies: "Technologies used: React, Ruby, Ruby on Rails",
   },
   {
     //first
     id: 2,
     // image: IMG1,
-    title: "QPQ",
-    github: "https://github.com/bdb2381/qpq-frontend",
-    demo: "",
+    title: "Message Me App",
+    github: "https://github.com/keeks5456/message_me_app",
+    technologies: "Technologies used: JavaScript, Ruby, HTML",
   },
   {
     //first
     id: 3,
     // image: IMG1,
-    title: "QPQ",
-    github: "https://github.com/bdb2381/qpq-frontend",
-    demo: "",
+    title: "Alpha Blog",
+    github: "https://github.com/keeks5456/alpha_blog",
+    technologies: "Technologies used: Ruby, SCSS, HTML, JavaScript",
   },
   {
     //first
     id: 4,
     // image: IMG1,
-    title: "QPQ",
-    github: "https://github.com/bdb2381/qpq-frontend",
-    demo: "",
+    title: "Recipes With Frank",
+    github: "https://github.com/alphaworks-react-team/RecipesWithFrank",
+    technologies:
+      "Technologies used: React, JavaScript, CSS, Style-components, HTML",
+  },
+  {
+    //first
+    id: 5,
+    // image: IMG1,
+    title: "Interactive Rating App",
+    github: "https://github.com/keeks5456/Interactive-rating-app",
+    technologies:
+      "Technologies used: React, JavaScript, CSS, Style-components, HTML",
   },
 ];
